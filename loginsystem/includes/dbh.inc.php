411f993c082fb186c3eb0f2ab8ffce504f0d3d90
mgroesink <mroesink@rocvantwente.nl>
2018-03-18 11:12:56 +0100

    Signup added

---
+++ loginsystem/includes/dbh.inc.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Marcel
+ * Date: 18-3-2018
+ * Time: 09:25
+ */
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "";
+$dbName = "loginsystem";
+
+$conn = mysqli_connect($dbServername , $dbUsername , $dbPassword , $dbName);
+
--- loginsystem/signup.php
+++ loginsystem/signup.php
@@ -4,6 +4,14 @@ include_once "header.php"
     <section class="main-container">
         <div class="main-wrapper">
             <h2>Sign up</h2>
+            <form class="signup-form" action="includes/sign.inc.php" method="post">
+                <input type="text" name="first" placeholder="Firstname">
+                <input type="text" name="last" placeholder="Lastname">
+                <input type="email" name="email" placeholder="E-mail">
+                <input type="text" name="uid" placeholder="Username">
+                <input type="password" name="pwd" placeholder="Password">
+                <button type="submit" name="submit">Sign up</button>
+            </form>
         </div>
     </section>
 <?php
